e6bcf086aee0be4cb6ee303ea783f42aead67c12
Rahul  Gupta <194687497+rguptaprofile@users.noreply.github.com>
2025-06-15 11:04:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@
   const successMsg = document.getElementById('response');
   const errorMsg = document.getElementById('error');
 
-  form.addEventListener('Send Message', (e) => {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const name = document.getElementById("name").value.trim();
